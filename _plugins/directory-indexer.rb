f90f9c1ac5f10ff69f56d25386b56039a217cdd4
TitusDoggo <work@aerofur.nz>
2023-03-01 23:29:54 +1300

    Update directory-indexer.rb

--- _plugins/directory-indexer.rb
+++ _plugins/directory-indexer.rb
@@ -1,1 +1,11 @@
+require "fileutils"
 
+module Jekyll
+    module DirectoryIndexer
+        def directory_index(path)
+            @site.static_files.select { |file| INCLUDED_EXTENSIONS.include? file.extname }
+        end
+    end
+end
+  
+Liquid::Template.register_filter(Jekyll::DirectoryIndexer)
